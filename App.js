5e16a5a1d3755ffbc724b4dcc9201bdf2d6a6052
VladimirZ05 <132680480+VladimirZ05@users.noreply.github.com>
2023-08-05 13:33:24 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { text: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const toggleComplete = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="app">
+      <h1>Список задач</h1>
+      <div className="add-task">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Введите новую задачу"
+        />
+        <button onClick={addTask}>Добавить</button>
+      </div>
+      <ul className="task-list">
+        {tasks.map((task, index) => (
+          <li key={index} className={task.completed ? 'completed' : ''}>
+            <span onClick={() => toggleComplete(index)}>{task.text}</span>
+            <button onClick={() => removeTask(index)}>Удалить</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
